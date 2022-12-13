cafdf2cb2855cf63a13824585a26b0722d8f4b41
Sky K <adamkunkel54@gmail.com>
2022-12-12 19:10:11 -0500

    update clean data R and main data
    reorganizing data, cleaning in a new way

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -3,25 +3,95 @@
 
 ### load libraries ###
 library(doSNOW); library(foreach); library(janitor); library(lubridate)
-library(sf); library(tidyverse); library(sp); library(spatialEco)
+library(sf); library(tidyverse); library(sp)
 
 ### Reading in and briefly cleaning the data ###
 
 # set working directory #
 setwd("../")
 
-# load the data #
+#### Read in ACLED data ####
 acled  = read.csv("./data/acled/1999-01-01-2021-12-31.csv") %>%
-  select(-c(event_id_cnty, event_id_no_cnty, actor1, assoc_actor_1,actor2,assoc_actor_2,region,
-            admin3,location,source,source_scale,notes,timestamp))
+  select(-c(data_id, event_id_cnty, event_id_no_cnty, actor1, assoc_actor_1,actor2,assoc_actor_2,region,
+            admin3,location,source,source_scale,notes,timestamp, iso, iso3, time_precision, admin1,
+            admin2)) %>%  
+  mutate(event_date = dmy(event_date), month = month(event_date), year = year(event_date))
 
+# make date into date-time object
+acled$event_date = lubridate::dmy(acled$event_date)
 
+# subset ACLED data to violence against civilians and dates from before 2019 and after 
+# 1999 to match RADPKO data (and to make analysis faster)
+acled = subset(acled, event_date < "2019-01-01" & event_date > "1999-01-01" & 
+                 event_type == "Violence against civilians" | event_type == "Explosions/Remote violence")
+acled = subset(acled, inter2 == 7 & inter1 == 1 | inter1 == 2 | inter1 == 3)
+
+### add VAC events and deaths by actor
+acled$event = 1
+acled$vac_gov_death_all = 0
+acled$vac_gov_death_all[acled$interaction == 17 | acled$interaction == 37] = 
+  acled$fatalities[acled$interaction == 17 | acled$interaction == 37]
+acled$vac_gov_death_any = 0
+acled$vac_gov_death_any[acled$interaction == 17 | acled$interaction == 37] = 1
+acled$vac_reb_death_all = 0
+acled$vac_reb_death_all[acled$interaction == 27] = acled$fatalities[acled$interaction == 27]
+acled$vac_reb_death_any = 0
+acled$vac_reb_death_any[acled$interaction == 27] = 1
+acled$vac_gov_event_all = 0
+acled$vac_gov_event_all[acled$interaction == 17 | acled$interaction == 37] = 
+  acled$event[acled$interaction == 17 | acled$interaction == 37]
+acled$vac_gov_event_any = 0
+acled$vac_gov_event_any[acled$interaction == 17 | acled$interaction == 37] = 1
+acled$vac_reb_event_all = 0
+acled$vac_reb_event_all[acled$interaction == 27] = acled$event[acled$interaction == 27]
+acled$vac_reb_event_any = 0
+acled$vac_reb_event_any[acled$interaction == 27] = 1
+acled$vac_gov_death_5 = 0
+acled$vac_gov_death_5[acled$interaction == 17 | acled$interaction == 37 & acled$fatalities >= 5] = 1
+acled$vac_reb_death_5 = 0
+acled$vac_reb_death_5[acled$interaction == 27 & acled$fatalities >= 5] = 1
+
+# change ACLED's classification of Abyei from admin1 unit to country unit to match RADPKO
+acled$country[acled$admin1=="Abyei"] = "Abyei"
+
+#### read in RADPKO data ####
 radpko = read.csv("./data/radpko/radpko_grid.csv")  %>%
   select(-c(west_pko,west_untrp,west_unpol,west_unmob,asian_pko,asian_untrp,asian_unpol,
             asian_unmob,afr_pko,afr_untrp,afr_unpol,afr_unmob)) %>%
   mutate(date = ymd(date),
          month = month(date),
          year = year(date))
+
+# Create a "treatment" indicator telling us if PKs existed in a certain grid at a certain time 
+radpko$t_ind = 0
+radpko$t_ind[radpko$units_deployed >= 1] = 1
+
+# change female PKs to proportion
+radpko$f_untrp.p = radpko$f_untrp/radpko$untrp
+radpko = radpko %>%
+  relocate(f_untrp.p, .after = untrp)
+radpko$f_unpol.p = radpko$f_unpol/radpko$unpol
+radpko = radpko %>%
+  relocate(f_unpol.p, .after = unpol)
+radpko$f_unmob.p = radpko$f_unmob/radpko$unmob
+radpko = radpko %>%
+  relocate(f_unmob.p, .after = unmob)
+
+# add proportions of each type to get total gender balance and then split treatment by balance
+radpko$gen.bal = radpko$f_untrp.p + radpko$f_unpol.p + radpko$f_unmob.p 
+radpko$t_bal = 0 # make balanced treatment indicator
+radpko$t_bal[radpko$gen.bal > median(radpko$gen.bal[radpko$t_ind==1])] = 1
+radpko$t_unbal = 0 # make un_balanced treatment indicator
+radpko$t_unbal[radpko$gen.bal < 0.06416706 & radpko$t_ind == 1] = 1
+# add variable denoting pk type by binary measure of treatment
+radpko$untrp_maj = 0
+radpko$untrp_maj[radpko$untrp > radpko$unpol & radpko$untrp > radpko$unmob] = 1
+radpko$unpol_maj = 0
+radpko$unpol_maj[radpko$unpol > radpko$untrp & radpko$unpol > radpko$unmob] = 1
+radpko$unmob_maj = 0
+radpko$unmob_maj[radpko$unmob > radpko$unpol & radpko$unmob > radpko$untrp] = 1
+
+#### read in PRIO data ####
 prio.static = read_csv("./data/prio/PRIO-GRID Static Variables - 2022-06-03.csv")
 prio.yearly = read_csv("./data/prio/PRIO-GRID Yearly Variables for 1999-2014 - 2022-06-03.csv")
 names(prio.static)[1] = "prio.grid" # rename for merging
@@ -34,34 +104,9 @@ prio.var = left_join(prio.yearly, prio.static, by = c("prio.grid"))
 prio.var$prio.grid = as.numeric(prio.var$prio.grid)
 rm(prio.static, prio.yearly)
 # change date-time
-acled$event_date = lubridate::dmy(acled$event_date)
-radpko$date = lubridate::ymd(radpko$date) 
+radpko$date = lubridate::ymd(radpko$date)
 
-# change female PKs to proportion
-radpko$f_untrp.p = radpko$f_untrp/radpko$untrp
-radpko = radpko %>%
-  relocate(f_untrp.p, .after = untrp)
-radpko$f_unpol.p = radpko$f_unpol/radpko$unpol
-radpko = radpko %>%
-  relocate(f_unpol.p, .after = unpol)
-radpko$f_unmob.p = radpko$f_unmob/radpko$unmob
-radpko = radpko %>%
-  relocate(f_unmob.p, .after = unmob)
-radpko$f_untrp = NULL
-radpko$f_unpol = NULL
-radpko$f_unmob = NULL
-
-# subset ACLED data to violence against civilians and dates from before 2019 and after 
-# 1999 to match RADPKO data (and to make analysis faster)
-acled = subset(acled, event_date < "2019-01-01" & event_date > "1999-01-01" & 
-                 event_type == "Violence against civilians" | event_type == "Explosions/Remote violence")
-acled = subset(acled, inter1 == 7 | inter2 == 7)
-# change ACLED's classification of Abyei from admin1 unit to country unit to match RADPKO
-acled$country[acled$admin1=="Abyei"] = "Abyei"
-
-
-
-### adding PRIO-grid numbers to ACLED data ###
+#### adding PRIO-grid numbers to ACLED data ####
 # let's start by aggregating into month-level units # 
 # first we need to aggregate data into PRIO-Grid's
 
@@ -69,79 +114,47 @@ acled$country[acled$admin1=="Abyei"] = "Abyei"
 # prio uses WGS84 CRS
 prio = st_read(dsn = "./data/prio", 
                layer = "priogrid_cell", 
-               stringsAsFactors = F) %>% 
-  mutate(gid = as.character(gid))
-
+               stringsAsFactors = F)
 names(prio)[1] = "prio.grid" # rename for merging
-prio$prio.grid = as.numeric(prio$prio.grid) # transform the column into numeric so we can join the data
 
-## join data together ##
-prio.rad = left_join(radpko, prio, by = "prio.grid") 
+# save the CRS
+proj_crs <- st_crs(prio)
 
-# transform both datasets into  spatial objects
-prio.sp = as(prio, Class = "Spatial") # 
-
-# assign crs system for ACLED data #
-wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-
-acled.sp <- SpatialPointsDataFrame(acled[15:14],         # reading in the dataframe as a spatial object
-                                   acled,                 # the R object to convert
-                                   proj4string = wgs84)   # assign a CRS 
-
-
-# run point.in.poly, which determines if a coordinate (ACLED data) exists within a polygon (PRIO)
-# if the coordinate exists inside of the grid, it applies the characteristics of the polygon
-# to the coordinate; i.e., the PRIO-grid number + coordinates + geometry is added to each 
-# event in the ACLED data
-b = point.in.poly(acled.sp, prio.sp, sp = TRUE, duplicate = TRUE)
-a = as.data.frame(b) # convert to dataframe
-
-
+# convert to sf
+acled <- st_as_sf(acled, coords = c("longitude", "latitude"), crs = proj_crs)
+acled <- st_join(acled, prio)
+a = as.data.frame(acled) # convert to dataframe
+a[,24:28] = NULL
+a$event_date = NULL
 
 ### Now that R knows which points of violence occur in which grids, let's aggregate the data ###
-
-# start by adding a dummy column so we know how many violent events occur in a grid per month
-a$event = 1
-
-# round daily event data down to the 1st of the month to match RADPKO data
-a$event_date <- floor_date(a$event_date, "month")
-
-# change variable types to prepare for merging
-prio.rad$date = as.Date(prio.rad$date, "%m/%d/%Y") # format dates
-a$prio.grid = as.character(a$prio.grid)
-prio.rad$prio.grid = as.character(prio.rad$prio.grid)
-
 # group fatalities and events by PRIO-grid & date
-a.ag = a %>%
-  group_by(prio.grid, event_date, inter1) %>%
-  summarize(fatalities = sum(fatalities), event = sum(event))
+# rearrange the columns to put the geo-locational data last
+a = a %>% 
+  relocate(c("year", "month"), .after = last_col())
 
-a.ag$prio.grid = as.character(a.ag$prio.grid)
-prio$prio.grid = as.character(prio$prio.grid)
-a.ag.prio = left_join(a.ag, prio, by = "prio.grid") 
+a <- a %>% 
+  group_by(prio.grid, year, month) %>% 
+  summarise(across(fatalities:vac_reb_death_5, sum)) %>% 
+  ungroup()
+
+a$vac_gov_event_any[a$vac_gov_event_any>=1] = 1
+a$vac_reb_event_any[a$vac_reb_event_any>=1] = 1
+a$vac_gov_death_any[a$vac_gov_death_any>=1] = 1
+a$vac_reb_death_any[a$vac_reb_death_any>=1] = 1
 
 # add id to ACLED data
-a.ag.prio <- tibble::rowid_to_column(a.ag.prio, "id")
-
-# rename event_date to date for merging
-names(a.ag.prio)[3] = "date"
+a <- tibble::rowid_to_column(a, "id")
 
 ## merge ##
-merged.data = left_join(prio.rad, a.ag.prio, by = c("prio.grid", "date",
-                                                    "geometry", "xcoord",
-                                                    "ycoord", "col", "row"))
-
-# extract year to merge prio yearly data
-merged.data$year = year(merged.data$date)
-merged.data$prio.grid = as.numeric(merged.data$prio.grid)
+merged.data = left_join(radpko, a, by = c("prio.grid", "year", "month"))
 
 # add various data to merged dataset
-merged.data = left_join(merged.data, prio.var, by = c("prio.grid", "row", "col",
-                                                         "xcoord", "ycoord", "year"))
+merged.data = left_join(merged.data, prio.var, by = c("prio.grid", "year"))
 
 # rearrange the columns to put the geo-locational data last
 merged.data = merged.data %>% 
-  relocate(c("xcoord", "ycoord", "col", "row", "geometry"), .after = last_col())
+  relocate(c("xcoord", "ycoord"), .after = last_col())
 
 # verify there are no missing observations
 sum(is.na(merged.data$mission)) 
@@ -152,18 +165,10 @@ a = merged.data
 rm(merged.data)
 gc()
 
-# Create a "treatment" indicator telling us if PKs existed in a certain grid at a certain time 
-a$t_ind = 0
-a$t_ind[a$units_deployed >= 1] = 1
-a$event.b = 0
-a$event.b[a$event>0] = 1
-a$death = 0
-a$death[a$fatalities>0] = 1
+
+
+# make control variable population density
 a$pop.dens = a$pop_gpw_sum / a$landarea 
-a$fate.5 = 0
-a$fate.5[a$fatalities > 4] = 1
-a$event.5 = 0
-a$event.5[a$event > 4] = 1
 # replace NAs w/ 0
 a$units_deployed[is.na(a$units_deployed)] <- 0
 a$countries_deployed[is.na(a$countries_deployed)] <- 0
@@ -180,51 +185,11 @@ a$f_unmob.p[is.nan(a$f_unmob.p)] <- 0
 a$fatalities[is.na(a$fatalities)] <- 0
 a$event[is.na(a$event)] <- 0
 a$mountains_mean[is.na(a$mountains_mean)] <- 0
-# add proportions of each type to get total gender balance and then split treatment by balance
-a$gen.bal = a$f_untrp.p + a$f_unpol.p + a$f_unmob.p 
-a$t_bal = 0 # make balanced treatment indicator
-a$t_bal[a$gen.bal > median(a$gen.bal[a$t_ind==1])] = 1
-a$t_unbal = 0 # make un_balanced treatment indicator
-a$t_unbal[a$gen.bal < 0.06416706 & a$t_ind == 1] = 1
-# add variable denoting pk type by binary measure of treatment
-a$untrp_maj = 0
-a$untrp_maj[a$untrp > a$unpol & a$untrp > a$unmob] = 1
-a$unpol_maj = 0
-a$unpol_maj[a$unpol > a$untrp & a$unpol > a$unmob] = 1
-a$unmob_maj = 0
-a$unmob_maj[a$unmob > a$unpol & a$unmob > a$untrp] = 1
 
-#### add OSV by distinct actors variables ####
-# violent events + binaries #
-a$inter1[is.na(a$inter1)] <- 0
-a$gov_event = 0
-a$gov_event[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4] = a$event[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4]
-a$gov_event.b = 0
-a$gov_event.b[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4] = a$event.b[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4]
-a$reb_event = 0
-a$reb_event[a$inter1 == 2] = a$event[a$inter1 == 2]
-a$reb_event.b = 0
-a$reb_event.b[a$inter1 == 2] = a$event.b[a$inter1 == 2]
+a <- a %>% 
+  mutate(across(fatalities:vac_reb_death_5, 
+                ~replace_na(.x, 0)))
 
-a$gov_event.5 = 0
-a$gov_event.5[a$gov_event.b == 1 & a$event.5 == 1] = 1
-a$reb_event.5 = 0
-a$reb_event.5[a$reb_event.b == 1 & a$event.5 == 1] = 1
-
-# fatalities + binaries #
-a$gov_death = 0
-a$gov_death[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4] = a$fatalities[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4]
-a$gov_death.b = 0
-a$gov_death.b[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4] = a$death[a$inter1 == 1 | a$inter1 == 3 | a$inter1 == 4]
-a$reb_death = 0
-a$reb_death[a$inter1 == 2] = a$fatalities[a$inter1 == 2]
-a$reb_death.b = 0
-a$reb_death.b[a$inter1 == 2] = a$death[a$inter1 == 2]
-
-a$gov_death.5 = 0
-a$gov_death.5[a$gov_death.b == 1 & a$fate.5 == 1] = 1
-a$reb_death.5 = 0
-a$reb_death.5[a$reb_death.b == 1 & a$fate.5 == 1] = 1
 
 # turn countries into numbers for mlm #
 a$ccode = 0
@@ -317,6 +282,15 @@ df = df %>%
 a = left_join(a, df, by = c("prio.grid", "date"))
 a$ucdp_deaths[is.na(a$ucdp_deaths)] <- 0
 a$ucdp_event[is.na(a$ucdp_event)] <- 0
+
+
+### reorganize and rename
+a <- a %>% 
+  select(-c(xcoord, ycoord)) %>% 
+  rename_at(vars(fatalities:vac_reb_death_5), 
+            function(x) paste0("acled_", x)) %>% 
+  rename_at(vars(units_deployed:f_unmob), function(x) paste0("radpko_", x)) %>% 
+  rename_at(vars(agri_gc:water_ih), function(x) paste0("prio_", x)) 
 
 # save RDS #
 
