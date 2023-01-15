9b81b6131b461e8b9a5378ed425ee90e82d8cb6c
Sky K <adamkunkel54@gmail.com>
2023-01-15 17:29:41 -0500

    updating clean_data and cleaned data
    also removing ACLED data since I'll be sticking with the GED's measurements in this paper

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -12,58 +12,6 @@ library(countrycode); library(geosphere)
 # set working directory #
 setwd("../")
 
-#### Read in ACLED data ####
-acled  = read.csv("./data/acled/1999-01-01-2021-12-31.csv") %>%
-  select(-c(data_id, event_id_cnty, event_id_no_cnty, actor1, assoc_actor_1,actor2,assoc_actor_2,region,
-            admin3,location,source,source_scale,notes,timestamp, iso, iso3, time_precision, admin1,
-            admin2)) %>%  
-  mutate(event_date = dmy(event_date), month = month(event_date), year = year(event_date))
-
-# subset ACLED data to violence against civilians and dates from before 2019 and after 
-# 1999 to match RADPKO data (and to make analysis faster)
-acled = subset(acled, event_date < "2019-01-01" & event_date > "1999-01-01" & 
-                 event_type == "Violence against civilians" | event_type == "Explosions/Remote violence")
-acled = subset(acled, inter2 == 7 & inter1 == 1 | inter1 == 2 | inter1 == 3)
-
-### add VAC events and deaths by actor
-acled$event = 1
-acled$vac_gov_death_all = 0
-acled$vac_gov_death_all[acled$interaction == 17 | acled$interaction == 37] = 
-  acled$fatalities[acled$interaction == 17 | acled$interaction == 37]
-acled$vac_gov_death_any = 0
-acled$vac_gov_death_any[acled$interaction == 17 | acled$interaction == 37] = 1
-acled$vac_reb_death_all = 0
-acled$vac_reb_death_all[acled$interaction == 27] = acled$fatalities[acled$interaction == 27]
-acled$vac_reb_death_any = 0
-acled$vac_reb_death_any[acled$interaction == 27] = 1
-acled$vac_gov_event_all = 0
-acled$vac_gov_event_all[acled$interaction == 17 | acled$interaction == 37] = 
-  acled$event[acled$interaction == 17 | acled$interaction == 37]
-acled$vac_gov_event_any = 0
-acled$vac_gov_event_any[acled$interaction == 17 | acled$interaction == 37] = 1
-acled$vac_reb_event_all = 0
-acled$vac_reb_event_all[acled$interaction == 27] = acled$event[acled$interaction == 27]
-acled$vac_reb_event_any = 0
-acled$vac_reb_event_any[acled$interaction == 27] = 1
-acled$vac_gov_death_5 = 0
-acled$vac_gov_death_5[acled$interaction == 17 | acled$interaction == 37 & acled$fatalities >= 5] = 1
-acled$vac_reb_death_5 = 0
-acled$vac_reb_death_5[acled$interaction == 27 & acled$fatalities >= 5] = 1
-
-### add VAC aggregated by actor
-acled$vac_death_all = 0
-acled$vac_death_all = acled$vac_gov_death_all + acled$vac_reb_death_all
-acled$vac_death_any = 0
-acled$vac_death_any[acled$vac_gov_death_any == 1 | acled$vac_reb_death_any == 1] = 1
-acled$vac_event_all = 0
-acled$vac_event_all = acled$vac_gov_event_all + acled$vac_reb_event_all
-acled$vac_event_any = 0
-acled$vac_event_any[acled$vac_gov_event_any == 1 | acled$vac_reb_event_any == 1] = 1
-
-
-# change ACLED's classification of Abyei from admin1 unit to country unit to match RADPKO
-acled$country[acled$admin1=="Abyei"] = "Abyei"
-
 #### read in RADPKO data ####
 radpko = read.csv("./data/radpko/radpko_grid.csv")  %>%
   select(-c(west_pko,west_untrp,west_unpol,west_unmob,asian_pko,asian_untrp,asian_unpol,
@@ -147,11 +95,10 @@ radpko =
 
 # create pko_supply calculating peacekeepers deployed to Africa by month and year
 pko_supply = radpko %>%
-  mutate(across(pko_deployed, 
-                ~replace_na(.x, 0))) %>%
-  select(month, year, pko_deployed) %>%
+  mutate(across(pko_deployed:f_pko_deployed, ~replace_na(.x, 0))) %>%
+  select(month, year, pko_deployed, m_pko_deployed, f_pko_deployed) %>%
   group_by(month,year) %>%
-  summarise(pko_africa = sum(pko_deployed))
+  summarise(pko_africa = sum(pko_deployed), m_pko_africa = sum(m_pko_deployed), f_pko_africa = sum(f_pko_deployed))
 radpko = left_join(radpko, pko_supply, by = c("month", "year"))
 # Fjelde et al. log distance to capital, whereas Ruggeri et al. measure it in kilometers
 # also measure PKO UN Africa in ten thousands, hard to tell how Ruggeri et al. measure
@@ -190,16 +137,53 @@ radpko <- radpko %>%
 
 # add proportions of each type to get total gender balance and then split treatment by balance
 radpko$t_bal = 0 # make balanced treatment indicator
-radpko$t_bal[radpko$f_prop > median(radpko$f_prop[radpko$t_ind==1])] = 1
+radpko$t_bal[radpko$f_prop > quantile(radpko$f_prop[radpko$t_ind == 1], prob=.75, type=1)] = 1
 radpko$t_unbal = 0 # make un_balanced treatment indicator
-radpko$t_unbal[radpko$f_prop <= median(radpko$f_prop[radpko$t_ind==1]) & radpko$t_ind == 1] = 1
-# add variable denoting pk type by binary measure of treatment
-radpko$untrp_maj = 0
-radpko$untrp_maj[radpko$untrp > radpko$unpol & radpko$untrp > radpko$unmob] = 1
-radpko$unpol_maj = 0
-radpko$unpol_maj[radpko$unpol > radpko$untrp & radpko$unpol > radpko$unmob] = 1
-radpko$unmob_maj = 0
-radpko$unmob_maj[radpko$unmob > radpko$unpol & radpko$unmob > radpko$untrp] = 1
+radpko$t_unbal[radpko$f_prop <= quantile(radpko$f_prop[radpko$t_ind == 1], prob=.75, type=1) & radpko$t_ind ==1] = 1
+
+##### Merge UCDP data #####
+# read in data
+dd = read.csv("./data/ucdp_ged/ucdp-actor-221.csv") %>%
+  rename(a_id = ActorId) %>%
+  select(c(a_id, Org)) # grab data so we can classify actors during OSV
+df = read.csv("./data/ucdp_ged/GEDEvent_v22_1.csv") %>%
+  # make the date variable a date type
+  mutate(date = ymd_hms(date_end)) %>%
+  mutate(month = month(date)) %>%
+  filter(type_of_violence == 3) %>%
+  select(c(date, month, year, side_a_new_id, priogrid_gid, deaths_civilians)) %>%
+  # rename variable for ease of merging
+  rename(prio.grid = priogrid_gid, ucdp_deaths = deaths_civilians) %>%
+  select(-c("date"))
+
+df = left_join(df, dd, by = c("side_a_new_id" = "a_id"))
+
+df = df %>%
+  group_by(prio.grid, year, month, Org) %>%
+  summarize(ucdp_deaths = sum(ucdp_deaths)) %>%
+  drop_na(Org) %>% 
+  ungroup()
+
+df$ucdp_gov_vac_5 = 0
+df$ucdp_gov_vac_5[df$Org == 4 & df$ucdp_deaths >= 5] = 1
+df$ucdp_gov_vac_all = 0
+df$ucdp_gov_vac_all[df$Org == 4] = df$ucdp_deaths[df$Org == 4]
+df$ucdp_reb_vac_5 = 0
+df$ucdp_reb_vac_5[df$Org == 1 & df$ucdp_deaths >= 5] = 1
+df$ucdp_reb_vac_all = 0 
+df$ucdp_reb_vac_all[df$Org==1] = df$ucdp_deaths[df$Org==1]
+
+df = df %>%
+  group_by(prio.grid, year, month) %>%
+  summarize(across(ucdp_gov_vac_5:ucdp_reb_vac_all, sum))
+
+a = left_join(radpko, df, by = c("prio.grid", "year", "month"))
+
+a <- a %>% 
+  mutate(across(ucdp_gov_vac_5:ucdp_reb_vac_all, 
+                ~replace_na(.x, 0)))
+
+rm(dd, df, countryref, pko_supply, before, after, radpko)
 
 #### read in PRIO data ####
 prio.static = read_csv("./data/prio/PRIO-GRID Static Variables - 2022-06-03.csv")
@@ -214,45 +198,8 @@ prio.var = left_join(prio.yearly, prio.static, by = c("prio.grid"))
 prio.var$prio.grid = as.numeric(prio.var$prio.grid)
 rm(prio.static, prio.yearly)
 
-
-#### adding PRIO-grid numbers to ACLED data ####
-# let's start by aggregating into month-level units # 
-# first we need to aggregate data into PRIO-Grid's
-
-# read in PRIO shape files from their website
-# prio uses WGS84 CRS
-prio = st_read(dsn = "./data/prio", 
-               layer = "priogrid_cell", 
-               stringsAsFactors = F)
-names(prio)[1] = "prio.grid" # rename for merging
-
-# save the CRS
-proj_crs <- st_crs(prio)
-
-# convert to sf
-acled <- st_as_sf(acled, coords = c("longitude", "latitude"), crs = proj_crs)
-acled <- st_join(acled, prio)
-a = acled %>%
-  as.data.frame() %>% # convert to dataframe
-  select(-c(xcoord, ycoord, col, row, geometry, event_date)) %>% 
-  relocate(c("year", "month"), .after = last_col()) %>% 
-  group_by(prio.grid, year, month) %>% 
-  summarise(across(fatalities:vac_event_any, sum)) %>% # aggregate violence by prio grid, month, and year
-  ungroup()
-
-# make binaries with counts > 0 as 1
-a$vac_gov_event_any[a$vac_gov_event_any>=1] = 1
-a$vac_reb_event_any[a$vac_reb_event_any>=1] = 1
-a$vac_gov_death_any[a$vac_gov_death_any>=1] = 1
-a$vac_reb_death_any[a$vac_reb_death_any>=1] = 1
-a$vac_death_any[a$vac_death_any >= 1] = 1
-a$vac_event_any[a$vac_event_any >= 1] = 1
-
 ## merge ##
-merged.data = left_join(radpko, a, by = c("prio.grid", "year", "month"))
-
-# add various data to merged dataset
-merged.data = left_join(merged.data, prio.var, by = c("prio.grid", "year"))
+merged.data = left_join(a, prio.var, by = c("prio.grid", "year"))
 
 # rearrange the columns to put the geo-locational data last
 merged.data = merged.data %>% 
@@ -288,44 +235,20 @@ a$pop.dens = a$pop_gpw_sum / a$landarea
 a$pop.dens<-ave(a$pop.dens,a$prio.grid,FUN=function(x) 
   ifelse(is.na(x), mean(x,na.rm=TRUE), x))
 
-# replace NAs w/ 0
-a$event[is.na(a$event)] <- 0
+# mountains are static, so any grids with missing values are 0
 a$mountains_mean[is.na(a$mountains_mean)] <- 0
-a <- a %>% 
-  mutate(across(fatalities:vac_event_any, 
-                ~replace_na(.x, 0)))
 
 # add summary violence 6 months prior 
-a = a %>% 
+a = a %>%
+  mutate(ucdp_vac_all = ucdp_reb_vac_all + ucdp_gov_vac_all) %>%
   group_by(prio.grid) %>% 
   mutate(viol_6 = 
-           lag(event, 6) + lag(event, 5) + lag(event, 4) + 
-           lag(event, 3) + lag(event, 2) + lag(event, 1)) %>%
-  relocate(viol_6, .after = event) %>%
+           lag(ucdp_vac_all, 6) + lag(ucdp_vac_all, 5) + lag(ucdp_vac_all, 4) + 
+           lag(ucdp_vac_all, 3) + lag(ucdp_vac_all, 2) + lag(ucdp_vac_all, 1)) %>%
+  relocate(viol_6, .after = ucdp_reb_vac_all) %>%
   ungroup()
 
-##### add a post treated variable #####
-# a <- a %>% 
-#   mutate(time = (year-1999)*(12) + month)
-# dd <- a %>% as.data.frame() %>% select(prio.grid, time, t_ind)
-# dd <- split(dd, f = dd$prio.grid)
-# dd <- lapply(dd, FUN = function(x){
-#   y <- x[which(x$t_ind == 1),]
-#   # create a "first treated" variable. needs to be 0 for untreated
-#   x$first_treated <- ifelse(nrow(y) == 0, 0, min(y$time))
-#   # create a "post treated" variable. needs to be 0 until treatment then 1
-#   x$post_treatment <- ifelse(x$first_treated != 0 & x$time >= x$first_treated, 
-#                              1, 0)
-#   # create a "treated" variable. needs to be 0 if control and 1 if treated
-#   x$treated <- ifelse(sum(x$t_ind, na.rm = T) > 0, 1, 0)
-#   x
-# })
-# dd <- do.call(rbind, dd)
-# dd <- dd[,c("prio.grid", "time", "first_treated", "post_treatment")]
-# # merge back to main df
-# a <- left_join(a, dd, by = c("prio.grid", "time"))
-
-##### add a lagged variable #####
+##### add a lagged variable of PKs deployed #####
 
 # sort again to make sure it works
 a = a[order(a$year, decreasing=FALSE), ] 
@@ -340,68 +263,19 @@ a <- a %>%                            # Add lagged column
 
 a$pko_lag[is.na(a$pko_lag)] <- 0 #we have all missions from their start, so any NAs are actually 0s
 
-##### Merge UCDP data #####
-# read in data
-dd = read.csv("./data/ucdp_ged/ucdp-actor-221.csv") %>%
-  rename(a_id = ï..ActorId) %>%
-  select(c(a_id, Org)) # grab data so we can classify actors during OSV
-df = read.csv("./data/ucdp_ged/GEDEvent_v22_1.csv") %>%
-  # make the date variable a date type
-  mutate(date = ymd_hms(date_end)) %>%
-  mutate(month = month(date)) %>%
-  filter(type_of_violence == 3) %>%
-  select(c(date, month, year, side_a_new_id, priogrid_gid, deaths_civilians)) %>%
-  # rename variable for ease of merging
-  rename(prio.grid = priogrid_gid, ucdp_deaths = deaths_civilians) %>%
-  select(-c("date"))
 
-df = left_join(df, dd, by = c("side_a_new_id" = "a_id"))
-
-df = df %>%
-  group_by(prio.grid, year, month, Org) %>%
-  summarize(ucdp_deaths = sum(ucdp_deaths)) %>%
-  drop_na(Org) %>% 
-  ungroup()
-
-df$ucdp_gov_vac_5 = 0
-df$ucdp_gov_vac_5[df$Org == 4 & df$ucdp_deaths >= 5] = 1
-df$ucdp_reb_vac_5 = 0
-df$ucdp_reb_vac_5[df$Org == 1 & df$ucdp_deaths >= 5] = 1
-df = df %>%
-  group_by(prio.grid, year, month) %>%
-  summarize(across(ucdp_gov_vac_5:ucdp_reb_vac_5, sum))
-
-a = left_join(a, df, by = c("prio.grid", "year", "month"))
-
-a <- a %>% 
-  mutate(across(ucdp_gov_vac_5:ucdp_reb_vac_5, 
-                ~replace_na(.x, 0)))
-
-rm(dd, df)
 
 ### reorganize and rename
 a <- a %>% 
   select(-c(xcoord, ycoord)) %>% 
-  rename_at(vars(fatalities:vac_event_any), function(x) paste0("acled_", x)) %>% 
   rename_at(vars(units_deployed:m_unmob), function(x) paste0("radpko_", x)) %>% 
-  rename_at(vars(agri_ih:pop.dens), function(x) paste0("prio_", x)) 
+  rename_at(vars(agri_ih:pop.dens), function(x) paste0("prio_", x))
 
 # remove useless columns
 a$acled_fatalities = NULL
 a$acled_event = NULL
 
-### create an "any fatalities" variable for ACLED
-a <- a %>% 
-  mutate(acled_fatalities_any = case_when(rowSums(across(
-    acled_vac_gov_death_all:acled_vac_reb_death_5)) > 0 ~ 1,
-    TRUE ~ 0)) %>%
-  relocate(acled_fatalities_any, .after = acled_vac_reb_death_5) %>% 
-  mutate(acled_fatalities_all = rowSums(across(
-    acled_vac_gov_death_all:acled_vac_reb_death_all))) %>%
-  relocate(acled_fatalities_all, .after = acled_fatalities_any)
-
-# save RDS #
-
+# save data #
 saveRDS(a, file = "./data/kunkel_cg.rds")
 
 # data fully merged, cleaned, and exported #
